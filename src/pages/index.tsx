7a8ef9da939fd5b840f91a4e00bda91aa4a21e99
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-06-11 10:46:17 +0200

    Added success message notification  on actions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+
 
 const IndexPage = () => {
-  const [notebook, setNotebook] = useState<any>();
+  
   return (
     <div className="flex flex-col justify-start items-center gap-5 sm:gap-3 h-full mt-12">
       <div className="mb-8">
